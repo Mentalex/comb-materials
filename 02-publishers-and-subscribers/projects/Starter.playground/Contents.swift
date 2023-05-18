7e4fe50a37cf37e38639d5469658c2802d260c07
Alex Tapia <alejandrovztapia@gmail.com>
2023-05-17 20:16:42 -0600

    Added example of `NotificationCenter` use, using observer mechanism.

--- 02-publishers-and-subscribers/projects/Starter.playground/Contents.swift
+++ 02-publishers-and-subscribers/projects/Starter.playground/Contents.swift
@@ -7,6 +7,16 @@ var subscriptions = Set<AnyCancellable>()
 example(of: "Publisher") {
   let myNotification = Notification.Name("MyNotification")
   let publisher = NotificationCenter.default.publisher(for: myNotification, object: nil)
+  
+  let center = NotificationCenter.default
+  
+  let observer = center.addObserver(forName: myNotification, object: nil, queue: nil) { _ in
+    print("Notification received!")
+  }
+  
+  center.post(name: myNotification, object: nil)
+  
+  center.removeObserver(observer)
 }
 
 /// Copyright (c) 2021 Razeware LLC
